05a46f72e5b76e422371a08f7f397e68bafa5618
jeiver <je.yande@udla.edu.co>
2017-02-02 02:10:23 -0500

    Initial commit

---
+++ app/src/main/java/com/jeiveryr/semana2coursera/DetalleElectronicos.java
@@ -0,0 +1,17 @@
+package com.jeiveryr.semana2coursera;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by usuario on 002, 02/02/2017.
+ */
+
+public class DetalleElectronicos extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.electronicos_detalle);
+    }
+}
---
+++ app/src/main/java/com/jeiveryr/semana2coursera/listaElectronicos.java
@@ -0,0 +1,13 @@
+package com.jeiveryr.semana2coursera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listaElectronicos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.electronicos_lista);
+    }
+}
