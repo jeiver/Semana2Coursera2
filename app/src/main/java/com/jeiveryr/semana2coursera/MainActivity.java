68496aad8de04ec709abbaf62b8f3cb783e79dd8
jeiver <je.yande@udla.edu.co>
2017-02-03 19:55:59 -0500

    Nuevos avances en el curso

--- app/src/main/java/com/jeiveryr/semana2coursera/MainActivity.java
+++ app/src/main/java/com/jeiveryr/semana2coursera/MainActivity.java
@@ -1,7 +1,11 @@
 package com.jeiveryr.semana2coursera;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.Touch;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        agregarFlotante();
+    }
+    public void agregarFlotante(){
+        FloatingActionButton miFlotante=(FloatingActionButton) findViewById(R.id.flabMiFlab);
+        miFlotante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),getResources().getString(R.string.mensajitoLoco),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
